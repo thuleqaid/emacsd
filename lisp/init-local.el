f9ac6237d4b729403713d917ee62d3a713f7c0bd
quan_tian <quant@kotei-info.com>
2015-11-16 18:04:42 +0800

    tab width

---
+++ lisp/init-local.el
@@ -0,0 +1,10 @@
+(setq indent-tabs-mode nil)
+(setq default-tab-width 4)
+(setq tab-width 4)
+(setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40
+      44 48 52 56 60 64 68 72 76 80 84 88 92 96))
+
+(global-ede-mode t)
+(semantic-mode 1)
+
+(provide 'init-local)
