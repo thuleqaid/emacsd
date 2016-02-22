1ebc8bafb1feebfb40d9ff23a30dc005be13424b
quan_tian <quant@kotei-info.com>
2016-02-22 16:26:26 +0800

    helm setting

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -81,9 +81,18 @@
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
 (global-unset-key (kbd "C-x c"))
 (global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "M-y") 'helm-show-killing-ring)
+(global-set-key (kbd "C-x b") 'helm-mini)
+
 (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
       helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
       helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-M-x-fuzzy-match                  t
+      helm-buffers-fuzzy-match              t
+      helm-recentf-fuzzy-match              t
+      helm-semantic-fuzzy-match             t
+      helm-imenu-fuzzy-match                t
       helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
       helm-ff-file-name-history-use-recentf t)
 (require-package 'helm-gtags)
