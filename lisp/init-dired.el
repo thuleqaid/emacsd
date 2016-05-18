51d1f1c3d275176f0cea73858611b59f0684cc30
quan_tian <quant@kotei-info.com>
2016-05-18 10:48:48 +0800

    open file with system default app in dired mode by "X"

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -8,10 +8,34 @@
 (let ((gls (executable-find "gls")))
   (when gls (setq insert-directory-program gls)))
 
+(defun thuleqaid/dired-open-in-external-app ()
+  "Open the current file or dired marked files in external app.
+The app is chosen from your OS's preference."
+  (interactive)
+  (let* ((file-list (dired-get-marked-files))
+         (file-count (length file-list))
+         (current-file (dired-get-filename))
+         (open-list (if (or (<= file-count 0) (> file-count 5))
+                        (list current-file)
+                      file-list)))
+    (cond
+     ((string-equal system-type "windows-nt")
+      (mapc
+       (lambda (fPath)
+         (w32-shell-execute "open" (replace-regexp-in-string "/" "\\" fPath t t))) file-list))
+     ((string-equal system-type "darwin")
+      (mapc
+       (lambda (fPath) (shell-command (format "open \"%s\"" fPath)))  file-list))
+     ((string-equal system-type "gnu/linux")
+      (mapc
+       (lambda (fPath) (let ((process-connection-type nil)) (start-process "" nil "xdg-open" fPath))) file-list)))
+    ))
+
 (after-load 'dired
   (require 'dired+)
   (require 'dired-sort)
   (diredp-toggle-find-file-reuse-dir 1)
+  (define-key dired-mode-map "X"    'thuleqaid/dired-open-in-external-app)
   (when (fboundp 'global-dired-hide-details-mode)
     (global-dired-hide-details-mode -1))
   (setq dired-recursive-deletes 'top)
