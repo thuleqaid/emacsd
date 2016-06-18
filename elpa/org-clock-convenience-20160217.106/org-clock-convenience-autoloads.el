a170b87d95b0724f347ec5b8ef56f9bdf4e9f93a
Thule Qaid <thuleqaid@gmail.com>
2016-06-19 06:35:30 +0800

    update plugins

--- elpa/org-clock-convenience-20160217.106/org-clock-convenience-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; org-clock-convenience-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("org-clock-convenience.el") (22331 9847
-;;;;;;  157972 192000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; org-clock-convenience-autoloads.el ends here
---
+++ elpa/org-clock-convenience-20160529.723/org-clock-convenience-pkg.el
@@ -0,0 +1,1 @@
+(define-package "org-clock-convenience" "20160529.723" "convenience functions for org time tracking" '((cl-lib "0.5") (org "8") (emacs "24.3")) :url "https://github.com/dfeich/org-clock-convenience" :keywords '("org"))
